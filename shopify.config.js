c024a6a12881fbcb8920008c8eeb7c4d8b5d7d50
MaysenAtShopify <maysen.greenwood@shopify.com>
2021-10-15 00:55:33 +1300

    Update shopify.config.js

--- shopify.config.js
+++ shopify.config.js
@@ -1,5 +1,5 @@
 export default {
   locale: 'en-us',
   storeDomain: 'maysensteststore.myshopify.com',
-  storefrontToken: '3d1e59f1fb51ceb26b70315c087c9b2e',
+  storefrontToken: '62c703bdf2354f1dd5a8559ffed83529',
 };
