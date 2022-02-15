c77362463f08caf317cb7a8507015cd4ab1b8c79
MaysenAtShopify <89561374+MaysenAtShopify@users.noreply.github.com>
2022-02-16 10:15:12 +1300

    Update storefront token

--- shopify.config.js
+++ shopify.config.js
@@ -1,5 +1,7 @@
 export default {
   locale: 'en-us',
   storeDomain: 'maysensteststore.myshopify.com',
-  storefrontToken: 'b011ee5bb0929b8f0c4fabdf3b874a1e',
+  storefrontToken: '55f7b5b916ce61a03f95686014ffd878',
 };
+
+// Previous storefrontToken: 'b011ee5bb0929b8f0c4fabdf3b874a1e',
